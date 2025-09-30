d6ff9ccfeb04d2c34c4d7e30aeb78e28f9e0dae3
akhona <akhonam850@gmail.com>
2025-09-30 16:55:04 +0200

    readmefile update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+//root element
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
